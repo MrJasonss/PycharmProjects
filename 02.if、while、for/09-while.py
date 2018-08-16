e1868b43a78f4e5f00c666f0253cad6f12fa921a
MrJasonss <MrJasonss@163.com>
2018-08-16 15:29:15 +0800

    Python 无自增、自减  无switch

--- 02.if、while、for/09-while.py
+++ 02.if、while、for/09-while.py
@@ -1,5 +1,8 @@
 i = 1
-
+'''
+    Python中没有do while    没有switch   没有++   没有--
+    https://blog.csdn.net/xyqzki/article/details/38414677    
+'''
 while i<=100:
     print("%d"%i)#print(i)
     i = i+1
