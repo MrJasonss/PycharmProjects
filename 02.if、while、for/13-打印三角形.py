ed408483bb0cb1b10968a1f3326916554f928182
MrJasonss <MrJasonss@163.com>
2018-08-16 17:18:13 +0800

    接受用户输入的三角形的高度   打印出三角形

--- 02.if、while、for/13-打印三角形.py
+++ 02.if、while、for/13-打印三角形.py
@@ -1,21 +1,23 @@
-i = 1
 
-while i<=5:
-
-    '''
+'''
     #从键盘中输入一个值,这个值用来控制这行中*的个数
     num = int(input("请输入这个行里的*的个数:"))
     
     j = 1
     while j<=num:
-        print("*", end="")
+        print("*", ed="")
         j+=1
-    '''
-    j = 1
-    while j<=i:
-        print("*", end="")
-        j+=1
+ '''
+i= 1
 
+num = input("请输入您要的三角形的高度：")
+num_int = int(num)
+while i<=num_int:
+    j=1
+    while j<=5:
+        print("*",end="");
+        j += 1
     print("")
-
     i+=1
+
+
